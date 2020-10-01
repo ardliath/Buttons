db8d8fc5b99161553228a1d3728faea39e7cda1e
Adam Griffiths <Adam.Griffiths@callcreditgroup.com>
2020-10-01 13:00:58 +0100

    Login

---
+++ Buttons/Buttons/Controllers/AccountController.cs
@@ -0,0 +1,46 @@
+﻿using Buttons.Models.Account;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Buttons.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return RedirectToAction("Login", "Account", new {  });
+        }
+
+
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(Login model)
+        {
+            if(!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            if(model.Username == "Adam" && model.Password == "password")
+            {
+                Session["UserID"] = Guid.NewGuid();
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ModelState.AddModelError("", "Invalid Login Attempt");
+                return View(model);
+            }
+        }
+    }
+}
